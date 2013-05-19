1ffcac3e82832ce3dea1938fcccd515f8f8728ea
skalerz <skalerz@yahoo.fr>
2013-05-19 03:17:25 +0200

    fin du Jour 8, sans modif du slugify pour accents...

--- src/Ens/JobeetBundle/Utils/Jobeet.php
+++ src/Ens/JobeetBundle/Utils/Jobeet.php
@@ -6,11 +6,18 @@ class Jobeet
 {
     public static function slugify($text)
     {
+         
+
         // replace all non letters or digits by -
         $text = preg_replace('/\W+/', '-', $text);
  
         // trim and lowercase
         $text = strtolower(trim($text, '-'));
+
+        if (empty($text))
+		  {
+		    return 'n-a';
+		  }
  
         return $text;
     }
